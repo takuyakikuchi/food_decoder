f10a474b234f4a0d52c150c2d7829f5a5cf4da1a
Satoru Nishizawa <satoru.nh@gmail.com>
2019-11-19 12:31:59 +0900

    user created

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ group :development do
   gem 'web-console', '>= 3.3.0'
 end
   gem 'dotenv-rails', groups: [:development, :test]
+  gem 'cloudinary', '~> 1.9.1'
+  gem 'carrierwave', '~> 1.2'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_restrictions
+  has_many :restrictions, through: :user_restrictions
+  mount_uploader :avatar, PhotoUploader
 end
