c737a29ff4ec545ea9f96cb3b99344175f2c0ac0
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-11-19 11:29:50 +0900

    created Restrictions Model and added associations and validations

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -1,2 +1,9 @@
 class Restriction < ApplicationRecord
+  has_many :user_restrictions
+  has_many :users, through: :user_restrictions
+  has_many :restriction_tags
+  has_many :products, through: :restriction_tags
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
