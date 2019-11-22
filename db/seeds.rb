e32f76c929dc76237f098a6a6ba249f17bd78bcf
Satoru Nishizawa <satoru.nh@gmail.com>
2019-11-22 15:56:48 +0900

    pics are many

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+include ActionView::Helpers
 # require 'csv'
 
 # puts 'Cleaning Products'
@@ -24,16 +25,31 @@
 
 # puts "...created #{Product.count} products"
 
-puts 'Cleaning Restrictions '
-Restriction.destroy_all
+# puts 'Cleaning Restrictions '
+# Restriction.destroy_all
 
-puts 'Creating restrictions......'
+# puts 'Creating restrictions......'
 
-restrictions = ['shrimp', 'crab', 'wheat', 'buckwheat', 'egg', 'milk', 'peanut', 'almond', 'abalone', 'squid', 'fish-egg', 'orange', 'cashew-nut', 'kiwi', 'meat', 'walnut', 'sesame', 'salmon', 'mackerel', 'soy', 'chicken', 'banana', 'pork', 'matsutake', 'peach', 'yam', 'apple', 'gelatin']
+# restrictions = ['shrimp', 'crab', 'wheat', 'buckwheat', 'egg', 'milk', 'peanut', 'almond', 'abalone', 'squid', 'fish-egg', 'orange', 'cashew-nut', 'kiwi', 'meat', 'walnut', 'sesame', 'salmon', 'mackerel', 'soy', 'chicken', 'banana', 'pork', 'matsutake', 'peach', 'yam', 'apple', 'gelatin']
 
-restrictions.each do |restriction|
-  Restriction.create!(name: restriction, photo: "restrictions_logo/#{restriction}.png")
-end
+# restrictions.each do |restriction|
+#   Restriction.create!(name: restriction, photo: "restrictions_logo/#{restriction}.png")
+# end
 
-puts "...created #{Restriction.count} restrictions"
+# puts "...created #{Restriction.count} restrictions"
 
+puts 'Creating curry seeds'
+
+product = Product.new(barcode: 4902970515842, name: 'Lawson Select Beef curry', ingredients: "Beef, sauteed onion, apple paste, beef extract, sauteed potherb (onion, carrot, celery, other), tomato paste, demiglace, sugar, curry powder, oil (plant origin), salt, gram powder, beef oil, chicken extract, ginger, red wine, butter, fond de veau, skim milk powder, dried garlic, colorant (caramel), condiment (amino acid), (contains wheat, milk extract, beef, chicken, pork, apple)")
+p image_path("/images/seed/fake_front.jpg")
+product.remote_front_photo_url = 'https://res.cloudinary.com/do6bcfrqi/image/upload/v1574405700/food-decoder/fake_front_pgsvzg.jpg'
+product.save!
+
+
+puts " Created : #{Product.last}"
+
+another = Product.new(barcode: 4979450793526, name: 'Hidagyu beef curry', ingredients: "Beef, vegetables(carrot, potato, ginger), wheat, roasted onion, beef oil, lard, milk product (contains soy), curry powder, sugar, apple paste, soy sauce(contains wheat and soy), beef extract, oister sauce(contains soy and apple), butter, salt, tomato paste, potherb, condiment (amino acid), colorant (caramel), acidulant, flavoring agent")
+another.remote_front_photo_url = 'https://res.cloudinary.com/do6bcfrqi/image/upload/v1574405701/food-decoder/hidagyu_front_xcrkwb.jpg'
+another.save!
+
+puts "Create: #{Product.last}"
