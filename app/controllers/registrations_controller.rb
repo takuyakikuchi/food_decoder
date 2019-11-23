bd32c78545e8978a4a7e7df791a2384f598df6c2
Takuya Kikuchi <51087054+takuyaKIKUCHI99@users.noreply.github.com>
2019-11-23 17:53:01 +0900

    delete registrations_controllers
    already merged

--- app/controllers/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-    protected
-    def after_sign_in_path_for(resource)
-      @user = current_user
-      edit_user_path(@user)
-    end
-end
