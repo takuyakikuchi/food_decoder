11c6f1c8e2f1dbf25df6374ccf3d9854afafd637
Satoru Nishizawa <satoru.nh@gmail.com>
2019-11-28 11:55:24 +0900

    ncm

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 include ActionView::Helpers
 require 'csv'
 
-# puts 'Cleaning Products'
-# History.destroy_all
-# Product.destroy_all
+puts 'Cleaning Products'
+History.destroy_all
+Product.destroy_all
 
-# puts 'Creating products......'
-# filepath = 'db/seed_csv.csv'
+puts 'Creating products......'
+filepath = 'db/seed_csv.csv'
 
-# CSV.foreach(filepath) do |row|
-#   # puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[3]}"
-#   product = Product.new(
-#     barcode: "#{row[0]}".to_i,
-#     name: "#{row[1]}",
-#     ingredients: "#{row[2]}")
-#     product.remote_front_photo_url ="#{row[3]}"
-#     product.save!
-# end
+CSV.foreach(filepath) do |row|
+  # puts "#{row[0]} | #{row[1]} | #{row[2]} | #{row[3]}"
+  product = Product.new(
+    barcode: "#{row[0]}".to_i,
+    name: "#{row[1]}",
+    ingredients: "#{row[2]}")
+    product.remote_front_photo_url ="#{row[3]}"
+    product.save!
+end
 
-# # --------- For barcode scan testing ---------
-# puts 'Creating candy seeds'
+# --------- For barcode scan testing ---------
+puts 'Creating candy seeds'
 
-# candy = Product.new(barcode: 4902888232374, name: "ramune", ingredients: "glucose, tapioca starch, milk calcium, acidulant, emulsilfier, aroma essence, (contains milk, constituent, and gelatin)")
-# candy.remote_front_photo_url = "https://res.cloudinary.com/nst-img/image/upload/v1574132180/product_test_1_heaobs.jpg"
-# candy.save!
+candy = Product.new(barcode: 4902888232374, name: "ramune", ingredients: "glucose, tapioca starch, milk calcium, acidulant, emulsilfier, aroma essence, (contains milk, constituent, and gelatin)")
+candy.remote_front_photo_url = "https://res.cloudinary.com/nst-img/image/upload/v1574132180/product_test_1_heaobs.jpg"
+candy.save!
 
-# puts " Created : #{Product.last}"
+puts " Created : #{Product.last}"
 
 puts 'Creating curry seeds'
 
